008332b2e9bb3ed220394bdfea8fa380202611bc
youngduck <dudejr5839@naver.com>
2022-07-08 16:59:04 +0900

    page구분,게시판1차제작

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import MainPage from './component/pages/MainPage';
 import Board from './component/pages/Board';
 import Write from './component/pages/Write';
 
+import {Pc,Mobile,Tablet} from './component/common/MediaQuery'
+
 const GlobalStyles = createGlobalStyle`
     ${reset};
 `;
@@ -16,11 +18,27 @@ function App() {
   return (
     <>
       <GlobalStyles/>
+      <Pc>
         <Routes>
           <Route path="/" element={<MainPage/>}></Route>
           <Route path="/board" element={<Board/>}></Route>
           <Route path="/write" element={<Write/>}></Route>
         </Routes>
+      </Pc>
+      <Mobile>
+        <Routes>
+          <Route path="/" element={<MainPage/>}></Route>
+          <Route path="/board" element={<Board/>}></Route>
+          <Route path="/write" element={<Write/>}></Route>
+        </Routes>
+      </Mobile>
+      <Tablet>
+        <Routes>
+          <Route path="/" element={<MainPage/>}></Route>
+          <Route path="/board" element={<Board/>}></Route>
+          <Route path="/write" element={<Write/>}></Route>
+        </Routes>
+      </Tablet>
     </>
 
     
